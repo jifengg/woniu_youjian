c3e858b79cb3392b143f8fecbc76bf5ae514fc2c
jifengg <11-dongdong@163.com>
2025-07-14 21:36:01 +0800

    增加图片菜单

--- background.js
+++ background.js
@@ -34,11 +34,18 @@ async function loadMenuConfig() {
       });
     }
 
+    if (config.image_contexts && config.image_contexts.length > 0) {
+      config.image_contexts.forEach(item => {
+        item.type = 'image';
+      });
+    }
+
     // 创建所有菜单项的汇总集合，方便后续查找
     config.all_items = [
       ...(config.text_contexts || []),
       ...(config.page_contexts || []),
-      ...(config.link_contexts || [])
+      ...(config.link_contexts || []),
+      ...(config.image_contexts || [])
     ];
 
     menuConfig = config;
@@ -49,6 +56,7 @@ async function loadMenuConfig() {
       text_contexts: [], 
       page_contexts: [], 
       link_contexts: [],
+      image_contexts: [],
       all_items: [] 
     };
     return menuConfig;
@@ -137,6 +145,27 @@ chrome.runtime.onInstalled.addListener(async () => {
   } else {
     console.warn('配置文件中没有找到有效的链接菜单项');
   }
+
+  // 创建图片右键菜单项
+  if (config.image_contexts && config.image_contexts.length > 0) {
+    // 创建图片右键菜单分隔符
+    chrome.contextMenus.create({
+      id: "image-separator",
+      type: "separator",
+      contexts: ["image"]
+    });
+
+    // 创建图片右键菜单组
+    config.image_contexts.forEach(item => {
+      chrome.contextMenus.create({
+        id: item.id,
+        title: item.title,
+        contexts: ["image"]
+      });
+    });
+  } else {
+    console.warn('配置文件中没有找到有效的图片菜单项');
+  }
 });
 
 // 处理上下文菜单点击事件
@@ -171,6 +200,12 @@ chrome.contextMenus.onClicked.addListener(async (info, tab) => {
       // 链接菜单，使用右键点击的链接URL
       if (!info.linkUrl) return;
       actionUrl = processUrl(menuItem.url, info.linkUrl);
+      break;
+
+    case 'image':
+      // 图片菜单，使用右键点击的图片URL
+      if (!info.srcUrl) return;
+      actionUrl = processUrl(menuItem.url, info.srcUrl);
       break;
 
     default:
